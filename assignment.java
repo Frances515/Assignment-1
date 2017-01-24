71759a854a90f243aa1a86b882c3317523811b96
Frances Antwi-Donkor <fad515@live.com>
2017-01-24 21:50:37 +0000

    initial

---
+++ assignment.java
@@ -0,0 +1,11 @@
+public class assignment{
+   
+  public static void main(String [] args){
+     if(args[1].equals("+")){
+      double num1 = Double.parseDouble(args[0]);
+      double num2 = Double.parseDouble(args[2]);
+      System.out.println(num1 + num2);
+
+      }
+  }
+}
